546b31aa91dd5cd2b9c62094bc5cf87885006e49
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2025-03-20 18:42:29 -0300

    fix: Bug where internal clock isnt 1Hz
    Also add 1Hz variant for judge

--- exp3/semaforo_1hz.vhdl
+++ exp3/semaforo_1hz.vhdl
@@ -21,50 +21,16 @@ architecture arch_semaforo of semaforo is
 		);
 	end component;
 
-	signal clk1024 : std_logic := '0';
-	signal clk1800 : std_logic := '0';
 	signal clk13_reset : std_logic := '0';
-	signal clk1800_reset : std_logic := '0';
-	signal clk1024_reset : std_logic := '0';
-	signal counter1024_o : std_logic_vector( 10 downto 0 ) := (others => '0');
-	signal counter1800_o : std_logic_vector( 10 downto 0 ) := (others => '0');
 	signal counter13_o : std_logic_vector( 3 downto 0 ) := (others => '0');
 	signal leds : std_logic_vector( 2 downto 0 ) := "100";
 begin
-	counter1024 : counter
-	generic map (
-		WIDTH => 11
-	)
-	port map (
-		clock => clk,
-		reset => clk1024_reset,
-		enable => '1',
-		load => '0',
-		up => '1',
-		data_i => ( others => '0' ),
-		data_o => counter1024_o
-	);
-
-	counter1800 : counter
-	generic map (
-		WIDTH => 11
-	)
-	port map (
-		clock => clk1024,
-		reset => clk1800_reset,
-		enable => '1',
-		load => '0',
-		up => '1',
-		data_i => ( others => '0' ),
-		data_o => counter1800_o
-	);
-
 	counter13 : counter
 	generic map (
 		WIDTH => 4
 	)
 	port map (
-		clock => clk1800,
+		clock => clk,
 		reset => clk13_reset,
 		enable => '1',
 		load => '0',
@@ -74,25 +40,6 @@ begin
 	);
 
 	process(clk)
-	begin
-
-		--if ( counter1024_o(10) = '1' ) then
-		if ( counter1024_o = "01111111111" ) then
-			clk1024 <= not clk1024;
-		end if;
-
-	end process;
-
-	process(clk1024)
-	begin
-		--if ( counter1800_o = "11100001000" ) then
-		if ( counter1800_o = "11100000111" ) then
-			clk1800 <= not clk1800;
-		end if;
-
-	end process;
-
-	process(clk1800)
 	begin
 
 		-- Desloca leds pra direita, uma vez só, se counter13_o = 5, 8, 13
@@ -107,8 +54,6 @@ begin
 	end process;
 
 	clk13_reset <= '1' when counter13_o = "1100" or reset = '1' else '0';
-	clk1800_reset <= '1' when clk1800 = '1' or reset = '1' else '0';
-	clk1024_reset <= '1' when clk1024 = '1' or reset = '1' else '0';
 
 	vermelho <= leds(0);
 	amarelo <= leds(1);
