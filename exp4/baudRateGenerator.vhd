9f8eacf3387ec2211179f5500194d445fe76db12
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2025-03-30 15:39:36 -0300

    add: BaudRateGenerator initial entity

---
+++ exp4/baudRateGenerator.vhd
@@ -0,0 +1,105 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_bit.all;
+
+entity baudRateGenerator is
+	port (
+		clock, reset 	: in std_logic;
+		divisor 			: in std_logic_vector( 15 downto 0 );
+		baudOut_n 		: out std_logic
+	);
+end baudRateGenerator;
+
+architecture arch_baud of baudRateGenerator is
+	component counter is
+		generic (
+			WIDTH : natural := 8 -- Size in bits
+		);
+		port (
+			clock, reset, enable, load, up : in std_logic;
+			data_i : in std_logic_vector( WIDTH-1 downto 0 );
+			data_o : out std_logic_vector( WIDTH-1 downto 0 )
+		);
+	end component;
+
+	signal primaryClock : std_logic := '0';
+	signal secondaryClock : std_logic := '0';
+
+	signal primaryCounter : unsigned( 15 downto 0 );
+	signal primaryCounter_o : std_logic_vector( 15 downto 0 );
+	signal primaryClk : std_logic := '0';
+	signal primaryCounter_reset : std_logic := '0';
+	signal primaryCounter_up : std_logic := '1';
+	signal primaryCounter_out : std_logic := '0';
+	signal secondaryCounter : unsigned( 15 downto 0 );
+	signal secondaryCounter_o : std_logic_vector( 15 downto 0 );
+	signal secondaryClk : std_logic := '0';
+	signal secondaryCounter_reset : std_logic := '0';
+	signal secondaryCounter_up : std_logic := '1';
+	signal secondaryCounter_out : std_logic := '0';
+begin
+	primaryCounter <= unsigned( divisor )/18432;
+	secondaryCounter <= unsigned( divisor )*100;
+
+	primCounter : counter
+	generic map (
+		WIDTH => 16
+	)
+	port map (
+		clock => clock,
+		reset => primaryCounter_reset,
+		enable => '1',
+		load => '0',
+		up => primaryCounter_up,
+		data_i => ( others => '0' ),
+		data_o => primaryCounter_o
+	);
+
+	segCounter : counter
+	generic map (
+		WIDTH => 16
+	)
+	port map (
+		clock => primaryClk,
+		reset => secondaryCounter_reset,
+		enable => '1',
+		load => '0',
+		up => secondaryCounter_up,
+		data_i => ( others => '0' ),
+		data_o => secondaryCounter_o
+	);
+
+	process(primaryCounter_o, reset)
+		-- Verifica se e o primeiro ciclo
+		variable firstRun : bit := '1';
+	begin
+		
+		-- Se reset for apertado
+		-- Coloca primaryCounter_up como 1, já que o contador resetado fica como 0000...
+		-- E set fisrtRun como 1 para que o clock nao flipe na instantaneamente
+		if ( reset = '1' ) then
+			primaryClk <= '0';
+			primaryCounter_reset <= '1';
+			primaryCounter_up <= '1';
+			firstRun := '1';
+		elsif ( unsigned(counter1024_o) = primaryClock ) then
+			-- Basicamente fica alternando entre contar de 0 até primaryCounter
+			-- e depois de primaryCounter até 0
+			-- Se chegar a primaryCounter ou 0, (excluindo o primeiro ciclo), flipa o clock
+			primaryClk <= not primaryClk;
+			primaryCounter_up <= '0';
+			firstRun := '0';
+		elsif ( counter1024_o = "00000000000" and firstRun = '0' ) then
+			primaryClk <= not primaryClk;
+			primaryCounter_up <= '1';
+		else
+			primaryCounter_reset <= '0';
+		end if;
+
+	end process;
+
+	-- Provavelmente ta errado
+	primaryClock <= 1843200/unsigned(divisor);
+	secondaryCounter <= divisor;
+
+end architecture;
