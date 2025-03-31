641b10baf193301eccccade30c0dfc3ef88bcb9f
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2025-03-31 13:00:52 -0300

    Just to sync with the lab PC

--- exp4/baudRateGenerator.vhd
+++ exp4/baudRateGenerator.vhd
@@ -22,28 +22,18 @@ architecture arch_baud of baudRateGenerator is
 		);
 	end component;
 
-	signal primaryClock : std_logic := '0';
-	signal secondaryClock : std_logic := '0';
-
-	signal primaryCounter : unsigned( 15 downto 0 );
-	signal primaryCounter_o : std_logic_vector( 15 downto 0 );
+	signal primaryCounter : std_logic_vector( 21 downto 0 );
+	signal primaryCounter_o : std_logic_vector( 21 downto 0 );
+	signal divisor_full_bit : std_logic_vector( 21 downto 0 ) := (others => '0');
 	signal primaryClk : std_logic := '0';
 	signal primaryCounter_reset : std_logic := '0';
 	signal primaryCounter_up : std_logic := '1';
 	signal primaryCounter_out : std_logic := '0';
-	signal secondaryCounter : unsigned( 15 downto 0 );
-	signal secondaryCounter_o : std_logic_vector( 15 downto 0 );
-	signal secondaryClk : std_logic := '0';
-	signal secondaryCounter_reset : std_logic := '0';
-	signal secondaryCounter_up : std_logic := '1';
-	signal secondaryCounter_out : std_logic := '0';
+	constant clockInBits : std_logic_vector( 21 downto 0 ) := "0111000010000000000000";
 begin
-	primaryCounter <= unsigned( divisor )/18432;
-	secondaryCounter <= unsigned( divisor )*100;
-
 	primCounter : counter
 	generic map (
-		WIDTH => 16
+		WIDTH => 22
 	)
 	port map (
 		clock => clock,
@@ -53,20 +43,6 @@ begin
 		up => primaryCounter_up,
 		data_i => ( others => '0' ),
 		data_o => primaryCounter_o
-	);
-
-	segCounter : counter
-	generic map (
-		WIDTH => 16
-	)
-	port map (
-		clock => primaryClk,
-		reset => secondaryCounter_reset,
-		enable => '1',
-		load => '0',
-		up => secondaryCounter_up,
-		data_i => ( others => '0' ),
-		data_o => secondaryCounter_o
 	);
 
 	process(primaryCounter_o, reset)
@@ -82,14 +58,14 @@ begin
 			primaryCounter_reset <= '1';
 			primaryCounter_up <= '1';
 			firstRun := '1';
-		elsif ( unsigned(counter1024_o) = primaryClock ) then
+		elsif ( primaryCounter_o = primaryCounter ) then
 			-- Basicamente fica alternando entre contar de 0 até primaryCounter
 			-- e depois de primaryCounter até 0
 			-- Se chegar a primaryCounter ou 0, (excluindo o primeiro ciclo), flipa o clock
 			primaryClk <= not primaryClk;
 			primaryCounter_up <= '0';
 			firstRun := '0';
-		elsif ( counter1024_o = "00000000000" and firstRun = '0' ) then
+		elsif ( primaryCounter_o = "00000000000" and firstRun = '0' ) then
 			primaryClk <= not primaryClk;
 			primaryCounter_up <= '1';
 		else
@@ -98,8 +74,17 @@ begin
 
 	end process;
 
+	process(divisor)
+	begin
+		primaryCounter <= std_logic_vector(to_unsigned(to_integer(unsigned(clockInBits) / unsigned(divisor_full_bit)),22));
+	end process;
+
+	divisor_full_bit( 15 downto 0) <= divisor;
+
 	-- Provavelmente ta errado
-	primaryClock <= 1843200/unsigned(divisor);
-	secondaryCounter <= divisor;
+	--primaryCounter <= to_unsigned(1843200, 22) / unsigned( divisor_unsig );
+	--primaryCounter <= std_logic_vector(to_unsigned(to_integer(unsigned(clockInBits) / unsigned(divisor_full_bit)),22));
+
+	baudOut_n <= primaryClk;
 
 end architecture;
