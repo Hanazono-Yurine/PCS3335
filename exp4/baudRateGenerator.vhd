8ef7b2c35970c1674edfe2e5ba92caaf00831fa1
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2025-04-01 00:56:01 -0300

    fix: Some variables names/Indentation

--- exp4/baudRateGenerator.vhd
+++ exp4/baudRateGenerator.vhd
@@ -3,32 +3,33 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity baudRateGenerator is
-    port(
-        clk       : in  std_logic;
-        reset       : in  std_logic;
-        divisor       : in  std_logic_vector(15 downto 0);
-        baudOut_n : out std_logic
-    );
+	port(
+		clock     : in  std_logic;
+		reset     : in  std_logic;
+		divisor		: in  std_logic_vector(15 downto 0);
+		baudOut_n : out std_logic
+	);
 end entity baudRateGenerator;
 
-architecture Behavioral of baudRateGenerator is
-    signal counter     : unsigned(15 downto 0) := (others => '0');
-    signal clk_out : std_logic := '0';
+architecture brg_arch of baudRateGenerator is
+	signal counter	: unsigned( 15 downto 0 ) := (others => '0');
+	signal clk_out 	: std_logic := '0';
 begin
 
-    process(clk, rst)
-    begin
-        if rst = '1' then
-            counter <= (others => '0');
-            clk_out <= '1';
-        elsif rising_edge(clk) then
-            if counter = shift_left(unsigned(divisor_val), 2) - 1 then
-                counter <= (others => '0');
-                clk_out <= not clk_out;
-            else
-                counter <= counter + 1;
-            end if;
-        end if;
-    end process;
-baudOut_n <= clk_out;
-end Behavioral;
+	process(clock, reset)
+	begin
+		if reset = '1' then
+			counter <= (others => '0');
+			clk_out <= '1';
+		elsif rising_edge(clock) then
+			if counter = shift_left(unsigned(divisor), 2) - 1 then
+				counter <= (others => '0');
+				clk_out <= not clk_out;
+			else
+				counter <= counter + 1;
+			end if;
+		end if;
+	end process;
+
+	baudOut_n <= clk_out;
+end brg_arch;
