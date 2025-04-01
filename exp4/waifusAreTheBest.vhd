2d907e1d20d6b3b3db0a5a57f1b326fd93a73999
Hanazono <142830863+Hanazono-Yurine@users.noreply.github.com>
2025-04-01 13:33:03 -0300

    add: Top-Level entity waifusAreTheBest for exp4

---
+++ exp4/waifusAreTheBest.vhd
@@ -0,0 +1,68 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_bit.all;
+
+entity waifusAreTheBest is
+	port (
+		clock 			: in std_logic;
+		reset 			: in std_logic;
+		serial_out 	: out std_logic
+	);
+end entity;
+
+architecture insides of waifusAreTheBest is
+
+	component transmitter is
+		port (
+			clock, reset: in std_logic;
+			serialOut: out std_logic
+		);
+	end component;
+
+	component baudRateGenerator is
+		port(
+			clock     : in  std_logic;
+			reset     : in  std_logic;
+			divisor		: in  std_logic_vector(15 downto 0);
+			baudOut_n : out std_logic
+		);
+	end component;
+
+	component ip_pll_50MHz is
+		port (
+			refclk   : in  std_logic := '0'; --  refclk.clk
+			rst      : in  std_logic := '0'; --   reset.reset
+			outclk_0 : out std_logic;        -- outclk0.clk
+			locked   : out std_logic         --  locked.export
+		);
+	end component;
+
+	signal clock_pll : std_logic := '0';
+	signal clock_brg : std_logic := '0';
+
+begin
+
+	baka: ip_pll_50MHz
+	port map (
+		refclk   => clock,
+		rst      => '0',
+		outclk_0 => clock_pll
+	);
+
+	fayeValentine : baudRateGenerator
+	port map (
+		clock  		=> clock_pll,
+		reset 		=> reset,
+		divisor 	=> std_logic_vector(to_unsigned(16,16)),
+		baudOut_n => clock_brg
+	);
+
+	shokoNishimiya: transmitter
+	port map (
+		clock     => clock_brg,
+		reset     => reset,
+		serialOut	=> serial_out
+	);
+
+end architecture;
