f1a8d87ffd44b25f9f7cab00b50e0cfbc0130728
Rafael Hipólito <64715008+RafaelHipolit@users.noreply.github.com>
2025-04-01 01:41:16 -0300

    Update transmissorDebug.vhd

--- exp4/transmissorDebug.vhd
+++ exp4/transmissorDebug.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity transmissorDebug is
 	port (
-		clock, reset: in std_logic;
+		clock50M, reset: in std_logic;
 		serialOut: out std_logic;
 
 		--debug
@@ -13,6 +13,24 @@ entity transmissorDebug is
 end entity;
 
 architecture rtl of transmissorDebug is
+
+	component ip_pll_50MHz is
+        port (
+            refclk   : in  std_logic := '0'; --  refclk.clk
+            rst      : in  std_logic := '0'; --   reset.reset
+            outclk_0 : out std_logic;        -- outclk0.clk
+            locked   : out std_logic         --  locked.export
+        );
+    end component;
+
+	component baudRateGenerator is
+		port(
+			clock       : in  std_logic;
+			reset       : in  std_logic;
+			divisor       : in  std_logic_vector(15 downto 0);
+			baudOut_n : out std_logic
+		);
+	end component baudRateGenerator;
 
 	component counter is
 		generic (
@@ -43,13 +61,29 @@ architecture rtl of transmissorDebug is
 	signal sig_desloca, en_desloca, reset_desloca: std_logic := '0';
 	signal counterClkDiv16: std_logic_vector(3 downto 0); 
 	signal counterDeslocaOut: std_logic_vector(4 downto 0);
-	signal clock9600: std_logic := '1';
+	signal clock9600, clock1_8MHz, clock153600: std_logic := '1';
 
 	--fsm
-    type state_type is (Sload, Sdesloca);
+    type state_type is (Sload, Sdesloca, Sreset);
     signal state, next_state: state_type := Sload;
 
 begin
+
+	--ip_pll
+    baka: ip_pll_50MHz
+    port map (
+        refclk   => clock50M,
+        rst      => '0',
+        outclk_0 => clock1_8MHz
+    );
+
+	baudrategenerator_inst: baudRateGenerator
+	port map (
+	  clock     => clock1_8MHz,
+	  reset     => reset,
+	  divisor   => "0000000000001100",
+	  baudOut_n => clock153600
+	);
 
 	reg: shiftregister
     generic map (
@@ -71,8 +105,8 @@ begin
         WIDTH => 4
     )
     port map (
-        clock  => clock,
-        reset  => reset,
+        clock  => clock153600,
+        reset  => '0',
         enable => '1',
         load   => '0',
         up     => '1',
@@ -94,16 +128,16 @@ begin
         data_o => counterDeslocaOut
     );
 
-	sig_desloca <= '1' when counterDeslocaOut = "10010" else
+	sig_desloca <= '1' when counterDeslocaOut = "11010" else -- 11010 = 11+15; 11 de enviar 11 bits; 15 de tempo de espera
 		'0'; 
-	en_desloca <= '0' when counterDeslocaOut = "10010" else
+	en_desloca <= '0' when counterDeslocaOut = "11010" else
 		'1';  
 
 	-- process padrao de procimo estado da fsm
 	process(clock9600, reset)
 	begin
 		if reset = '1' then
-		state <= Sload;
+			state <= Sreset;
 		elsif rising_edge(clock9600) then
 			state <= next_state;
 		end if;
@@ -111,11 +145,17 @@ begin
 
 	-- logica proximo estado
 	next_state <=
+		Sload when (state = Sreset) else
 		Sdesloca when (state = Sload) else
+		Sdesloca when (state = Sdesloca and sig_desloca = '0') else
 		Sload when (state = Sdesloca and sig_desloca = '1') else
 		state;
 
-		sig_regControl <= "11" when state = Sload else "01";
+	-- oq fazer em cada estado
+		sig_regControl <= "11" when state = Sload else 
+						"01" when state = Sdesloca else
+						"00" when state = Sreset;
+
 		reset_desloca <= '0' when state = Sdesloca else '1';
 
 	clock9600 <= counterClkDiv16(3);
