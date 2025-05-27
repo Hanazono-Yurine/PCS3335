cd91d5d7e2b3d6b4de4d7e2f69a585d8719740b7
Rafael Hipólito <64715008+RafaelHipolit@users.noreply.github.com>
2025-05-27 00:28:27 -0300

    finish mode1 base code

--- projeto/mode1.vhd
+++ projeto/mode1.vhd
@@ -62,6 +62,15 @@ architecture rtl of mode1 is
 		);
 	end component;
 
+    component bcd_asciiConverter is
+        port (
+            ascii_i : in std_logic_vector(6 downto 0); 
+            bcd_i : in std_logic_vector(3 downto 0); 
+
+            ascii_o : out std_logic_vector(6 downto 0); 
+            bcd_o : out std_logic_vector(3 downto 0) 
+        );
+    end component;
 
 	-- ============================================= SIGNAL =============================================
 
@@ -73,13 +82,21 @@ architecture rtl of mode1 is
 
     -- dataTempCounter
     signal dataTempCounterClock, dataTempCounterUp : std_logic := '0';
-	signal dataTempCounterValue : std_logic_vector(2 downto 0) := (others => '0') ; 
+	signal dataTempCounterValue : std_logic_vector(2 downto 0) := (others => '0') ;
+    signal dataTempCounterReset : std_logic := '0'; 
 
     -- dataTemp reg
     signal dataTempIn, dataTempOut : std_logic_vector (20 downto 0);
+    signal dataTempReset : std_logic := '0'; 
+    signal shiftDataTemp : integer := 0;
+
+    -- bcd_asciiConverter
+    signal bcd_o : std_logic_vector (3 downto 0);
+    signal ascii_o0, ascii_o1, ascii_o2, ascii_o3, ascii_o4, ascii_o5 : std_logic_vector (6 downto 0);
+    
 
     -- ============================================= FSM STATES =============================================
-    type state_type is (S_ready, S_storeTemp, S_deleteTemp, S_storeMemory, S_freeze2);
+    type state_type is (S_ready, S_storeTemp, S_update1, S_deleteTemp, S_update2, S_storeMemory, S_update3, S_freeze2);
     signal state, next_state: state_type := S_ready;
 
 	
@@ -103,12 +120,18 @@ begin
 		S_storeTemp   when (state = S_ready and keyNumberPressed = '1')   else	-- apertou algum digito
 		S_deleteTemp  when (state = S_ready and ascii = "1000011")   else -- apertou C
 		S_storeMemory when (state = S_ready and ascii = "1000101")   else -- apertou E
-		S_freeze2   when (state = S_storeTemp)   else
-        S_freeze2   when (state = S_deleteTemp)  else
-        S_freeze2   when (state = S_storeMemory) else
+		S_update1   when (state = S_storeTemp)   else
+        S_freeze2   when (state = S_update1)   else
+        S_update2   when (state = S_deleteTemp)  else
+        S_freeze2   when (state = S_update2)   else
+        S_update3   when (state = S_storeMemory) else
+        S_freeze2   when (state = S_update3)   else
 		S_freeze2   when (state = S_freeze2 and AchievedWantedValueFreezeCounter = '0' ) else 
 		S_ready     when (state = S_freeze2 and AchievedWantedValueFreezeCounter = '1') else
 		state;
+
+    -- S_update1 faz dataTempCounter++
+    -- S_update1 faz dataTempCounter--
 
     -- ============================================= LOGIC =============================================
 
@@ -154,7 +177,7 @@ begin
     )
     port map (
       clock       => clock,
-      reset       => reset,
+      reset       => dataTempReset,
       serial_i    => '0',
       loadOrShift => "11",
       data_i      => dataTempIn,
@@ -162,6 +185,37 @@ begin
       serial_o_r  => open,
       serial_o_l  => open
     );   
+
+    bcd_asciiConverter_inst: bcd_asciiConverter
+    port map(
+        ascii_i => ascii,
+        bcd_i => "0000",
+        ascii_o => open,
+        bcd_o => bcd_o
+    );
+    
+    dataTempIn(3 downto 0) <=   bcd_o when state = S_storeTemp else 
+                                    "0000"      when state = S_deleteTemp else 
+                                    dataTempOut(3 downto 0);
+
+    dataTempIn(7 downto 4) <=   bcd_o when state = S_storeTemp else 
+                                    "0000"      when state = S_deleteTemp else 
+                                    dataTempOut(7 downto 4);
+
+    dataTempIn(11 downto 8) <=  bcd_o when state = S_storeTemp else 
+                                    "0000"       when state = S_deleteTemp else 
+                                    dataTempOut(11 downto 8);
+
+    dataTempIn(15 downto 12) <= bcd_o when state = S_storeTemp else 
+                                    "0000"       when state = S_deleteTemp else 
+                                    dataTempOut(15 downto 12);
+
+    dataTempIn(19 downto 16) <= bcd_o when state = S_storeTemp else 
+                                    "0000"       when state = S_deleteTemp else 
+                                    dataTempOut(19 downto 16);  
+
+    --shiftDataTemp <= 4*to_integer(unsigned(dataTempCounterValue));                                                               
+    --dataTempIn(3 + shiftDataTemp downto 0 + shiftDataTemp) <= bcd_o when state = S_storeTemp else dataTempOut(3 + shiftDataTemp downto 0 + shiftDataTemp); -- nao gosto disso
     
     data_temp_size: counter -- conta quantos digitos estao armazenados no reg reg data_temp
     generic map (
@@ -169,7 +223,7 @@ begin
     )
     port map (
         clock  => dataTempCounterClock,
-        reset  => reset,
+        reset  => dataTempCounterReset,
         enable => '1',
         load   => '0',
         up     => dataTempCounterUp,
@@ -177,5 +231,82 @@ begin
         data_o => dataTempCounterValue
     );
 
+    dataTempCounterClock <= '1' when state = S_update1 or state = S_update2 else '0'; -- da um pulso de clock quando ta nos estados update
+
+    dataTempCounterUp <= '1' when state = S_update1 else
+                        '0' when state = S_update1 else
+                        '1'; 
+    
+
+    -- armazanar na memoria
+    memoryDataInMode1 <= dataTempOut;
+
+    memPosMode1 <= to_integer(unsigned(stackSizeCounterValue));  
+
+    wrMode1 <= '1' when state = S_storeMemory else '0';
+
+    stackSizeCounterClockMode1 <= '1' when state = S_update3 else '0'; -- da um pulso de clock
+    stackSizeCounterUpMode1 <= '1';
+
+    dataTempReset <= '1' when reset = '1' or state = S_update3 else '0';
+
+    dataTempCounterReset <= '1' when reset = '1' or state = S_update3 else '0';
+
+
+    -- exibir conteudo do data_temp nos displays
+    bcd_asciiConverter_inst0: bcd_asciiConverter
+    port map(
+        ascii_i => "0000000",
+        bcd_i => dataTempOut(3 downto 0),
+        ascii_o => ascii_o0,
+        bcd_o => open
+    );
+
+    bcd_asciiConverter_inst1: bcd_asciiConverter
+    port map(
+        ascii_i => "0000000",
+        bcd_i => dataTempOut(7 downto 4),
+        ascii_o => ascii_o1,
+        bcd_o => open
+    );
+
+    bcd_asciiConverter_inst2: bcd_asciiConverter
+    port map(
+        ascii_i => "0000000",
+        bcd_i => dataTempOut(11 downto 8),
+        ascii_o => ascii_o2,
+        bcd_o => open
+    );
+
+    bcd_asciiConverter_inst3: bcd_asciiConverter
+    port map(
+        ascii_i => "0000000",
+        bcd_i => dataTempOut(15 downto 12),
+        ascii_o => ascii_o3,
+        bcd_o => open
+    );
+
+    bcd_asciiConverter_inst4: bcd_asciiConverter
+    port map(
+        ascii_i => "0000000",
+        bcd_i => dataTempOut(19 downto 16),
+        ascii_o => ascii_o4,
+        bcd_o => open
+    );
+
+    bcd_asciiConverter_inst5: bcd_asciiConverter
+    port map(
+        ascii_i => "0000000",
+        bcd_i => "0000",
+        ascii_o => ascii_o5,
+        bcd_o => open
+    );
+
+    display7seg1Mode1 <= ascii_o0;
+    display7seg2Mode1 <= ascii_o1;
+    display7seg3Mode1 <= ascii_o2;
+    display7seg4Mode1 <= ascii_o3;
+    display7seg5Mode1 <= ascii_o4;
+    display7seg6Mode1 <= ascii_o5;
 
 end architecture;
